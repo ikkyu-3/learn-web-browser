83762892fbc784552e92d388d2dab84fbbebc77d
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-11-19 01:36:17 +0900

    [change] データベース構築の内容をハンズオン

--- indexedDB/using-indexedDB/index.js
+++ indexedDB/using-indexedDB/index.js
@@ -1,15 +1,28 @@
-let db;
+// データベース構築
+const customerData = [
+  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+];
+const dbName = "the_name";
 
-const request = indexedDB.open("MyTestDatabase");
+const request = indexedDB.open(dbName, 2);
+
 request.onerror = event => {
-  console.log("request: error", event);
+  console.error(event);
 };
 
-request.onsuccess = event => {
-  console.log("request: success");
-  db = event.target.result;
+request.onupgradeneeded = event => {
+  const db = event.target.result;
 
-  db.onerror = e => {
-    console.error(e);
+  const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+  objectStore.createIndex("name", "name", { unique: false });
+  objectStore.createIndex("email", "email", { unique: true });
+  objectStore.transaction.oncomplete = () => {
+    const customerObjectStore = db
+      .transaction("customers", "readwrite")
+      .objectStore("customers");
+
+    customerData.forEach(data => customerObjectStore.add(data));
   };
 };
