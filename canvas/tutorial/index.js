744d254d7afed74d6f44616753dfefbf2765065e
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2020-01-16 23:56:01 +0900

    [add] canvasディレクトリ作成

---
+++ canvas/tutorial/index.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("tutorial")
+const ctx = canvas.getContext("2d")
+
+ctx.fillStyle = "rpb(200, 0, 0)"
+ctx.fillRect(10, 10, 50, 50)
+
+ctx.fillStyle = "rgba(0, 0, 200, 0.5)"
+ctx.fillRect(30, 30, 50, 50)
